38dafb84e7a14a5469e98a47b41207098daf5719
pmoilane <petteri.j.moilanen@student.oulu.fi>
2026-08-18 03:24:15 +0300

    e2e test for individual pokemon

--- e2e-tests/e2e.spec.js
+++ e2e-tests/e2e.spec.js
@@ -1,4 +1,4 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
@@ -9,5 +9,14 @@ describe('Pokedex', () => {
         'Pokémon and Pokémon character names are trademarks of Nintendo.',
       ),
     ).toBeVisible()
+  })
+  describe('When on front page', () => {
+    beforeEach(async ({ page }) => {
+      await page.goto('')
+    })
+    test('individual pokemon can be opened', async ({ page }) => {
+      await page.getByText('squirtle').click()
+      await expect(page.getByText('torrent')).toBeVisible()
+    })
   })
 })
